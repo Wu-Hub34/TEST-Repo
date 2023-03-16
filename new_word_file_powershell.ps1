bbae18c6d58e7c03a74231706fefba5cce580b55
Goce Stoilov <goce@joker-it.ch>
2023-03-16 10:14:34 +0100

    Add DAte Time

--- new_word_file_powershell.ps1
+++ new_word_file_powershell.ps1
@@ -1,5 +1,6 @@
 #Create Word File on Desktop
-New-Item "C:\temp\test.txt"
+$date = Get-DAte -Format FileDateTime
+New-Item "C:\temp\test-$date.txt"
 <# $word = New-Object -ComObject Word.Application
 $doc = $word.Documents.Add()
 $doc.SaveAs("$env:userprofile\Desktop\testOBTScript.docx")
